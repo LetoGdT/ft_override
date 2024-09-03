da9861957fe471cf4590d71dbf70751ae7c80399
Leto Gaudet dit trafit <lgaudet-@z3r6p4.42lyon.fr>
2024-09-03 17:24:58 +0200

    fin de level05

--- level05/source.c
+++ level05/source.c
@@ -1,17 +1,14 @@
 #include <defs.h>
+#include <string.h>
 
-int __cdecl __noreturn main(int argc, const char **argv, const char **envp)
+int main(int argc, const char **argv)
 {
   char s[100]; // [esp+28h] [ebp-70h] BYREF
   unsigned int i; // [esp+8Ch] [ebp-Ch]
 
   i = 0;
   fgets(s, 100, stdin);
-  for ( i = 0; i < strlen(s); ++i )
-  {
-    if ( s[i] > 64 && s[i] <= 90 )
-      s[i] ^= 0x20u;
-  }
+  tolower(s);
   printf(s);
   exit(0);
 }
