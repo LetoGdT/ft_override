d357f339ab96c9c1f724f631910ba99f42d692a9
Leto Gaudet dit trafit <lgaudet-@z3r6p4.42lyon.fr>
2024-09-04 11:25:11 +0200

    correctif de la source

--- level06/source.c
+++ level06/source.c
@@ -1,14 +1,14 @@
 #include <defs.h>
 
-int auth(char *s, int a2)
+int auth(char *login, int password)
 {
   int i; // [esp+14h] [ebp-14h]
   int v4; // [esp+18h] [ebp-10h]
-  int v5; // [esp+1Ch] [ebp-Ch]
+  int login_len; // [esp+1Ch] [ebp-Ch]
 
-  s[strcspn(s, "\n")] = 0;
-  v5 = strnlen(s, 32);
-  if ( v5 <= 5 )
+  login[strcspn(login, "\n")] = 0;
+  login_len = strnlen(login, 32);
+  if ( login_len <= 5 )
     return 1;
   if ( ptrace(PTRACE_TRACEME, 0, 1, 0) == -1 )
   {
@@ -19,37 +19,38 @@ int auth(char *s, int a2)
   }
   else
   {
-    v4 = (s[3] ^ 0x1337) + 6221293;
-    for ( i = 0; i < v5; ++i )
+    v4 = (0x1300 + (login[3] ^ 0x37)) + 6221293;
+    for ( i = 0; i < login_len; ++i )
     {
-      if ( s[i] <= 31 )
+      if ( login[i] < ' ' )
         return 1;
       v4 += (v4 ^ (unsigned int)s[i]) % 0x539;
     }
-    return a2 != v4;
+    return password != v4;
   }
 }
 
 int main(int argc, const char **argv)
 {
-  int v4; // [esp+2Ch] [ebp-24h] BYREF
-  char s[28]; // [esp+30h] [ebp-20h] BYREF
-  unsigned int v6; // [esp+4Ch] [ebp-4h]
+  int password; // [esp+2Ch] [ebp-24h] BYREF
+  char login[28]; // [esp+30h] [ebp-20h] BYREF
+  unsigned int stack_canary; // [esp+4Ch] [ebp-4h]
 
-  v6 = __readgsdword(0x14u);
+  stack_canary = __readgsdword(0x14u);
   puts("***********************************");
   puts("*\t\tlevel06\t\t  *");
   puts("***********************************");
   printf("-> Enter Login: ");
-  fgets(s, 32, stdin);
+  fgets(login, 32, stdin);
   puts("***********************************");
   puts("***** NEW ACCOUNT DETECTED ********");
   puts("***********************************");
   printf("-> Enter Serial: ");
-  __isoc99_scanf(&unk_8048A60, &v4);
-  if ( auth(s, v4) )
+  scanf(&unk_8048A60, &password);
+  if ( auth(login, password) )
     return 1;
   puts("Authenticated!");
   system("/bin/sh");
+  verify(stack_canary)
   return 0;
 }
